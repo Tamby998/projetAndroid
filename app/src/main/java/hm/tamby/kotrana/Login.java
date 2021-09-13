a2bd3e8ad4ec1a0299355ea99c7a5584a4747561
therihar@etceterum <therihar@etceterum.net>
2021-09-13 08:51:46 +0300

    Projet Android Kotrana

---
+++ app/src/main/java/hm/tamby/kotrana/Login.java
@@ -0,0 +1,30 @@
+package hm.tamby.kotrana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        ImageView imageView = findViewById(R.id.ivFacebook) ;
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Registration.class) ;
+                startActivity(intent) ;
+//                Toast.makeText(getApplicationContext(), "Click" , Toast.LENGTH_SHORT).show() ;
+            }
+        });
+
+    }
+
+}
